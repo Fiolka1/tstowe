ac95436557412f238d21dbe8df6b27d5be772f6d
Fiolka1 <matihati60@gmail.com>
2024-02-14 11:47:47 +0100

    duzo zmian

--- src/app/ryby/ryby.component.ts
+++ src/app/ryby/ryby.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-ryby',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,NgForOf],
   templateUrl: './ryby.component.html',
   styleUrl: './ryby.component.css'
+ 
 })
 export class RybyComponent {
-  ryby:string[]=["łosoś","dorsz","karp","karaś"];
+  ryby=["łosoś","dorsz","karp","karaś"];
+  karasisko=2010;
+  /**
+   * name
+   */
+  public rybenki() {
+    for (let index = 0; index < 3; index++) {
+      document.write(this.ryby[index]+"<br>");
+      
+    }
+  }
 }
